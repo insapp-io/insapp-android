dfd217b621d4fb85ce458f2de5535f3846bae740
Thomas Bouvier <tomatrocho@gmail.com>
2016-11-13 16:14:14 +0100

    Added recycler view to post activity

--- app/src/main/java/fr/insapp/insapp/PostActivity.java
+++ app/src/main/java/fr/insapp/insapp/PostActivity.java
@@ -2,6 +2,9 @@ package fr.insapp.insapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 /**
  * Created by thoma on 12/11/2016.
@@ -9,9 +12,20 @@ import android.support.v7.app.AppCompatActivity;
 
 public class PostActivity extends AppCompatActivity {
 
+    public Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter();
+        recyclerView.setAdapter(adapter);
     }
 }

539b122a833e401e222eddc58b4dd08fbea15e63
Thomas Bouvier <tomatrocho@gmail.com>
2016-12-19 16:55:25 +0100

    Added settings activity

--- app/src/main/java/fr/insapp/insapp/SettingsActivity.java
+++ app/src/main/java/fr/insapp/insapp/SettingsActivity.java
@@ -1,27 +1,36 @@
 package fr.insapp.insapp;
 
 import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.design.widget.AppBarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
 /**
  * Created by thomas on 15/12/2016.
  */
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_settings);
-        if (toolbar != null) {
-            setSupportActionBar(toolbar);
-            getSupportActionBar().setHomeButtonEnabled(true);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
+        LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent().getParent().getParent();
+        AppBarLayout toolbar = (AppBarLayout) LayoutInflater.from(this).inflate(R.layout.settings_toolbar, root, false);
+        root.addView(toolbar, 0); // insert at top
+
+        ((Toolbar) toolbar.getChildAt(0)).setNavigationOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        addPreferencesFromResource(R.xml.preferences);
     }
 
     @Override

49138996a16a254596b8ba72bb2fa8112e1bf502
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-01-23 10:53:47 +0100

    Minor tweaks

--- app/src/main/java/fr/insapp/insapp/ClubActivity.java
+++ app/src/main/java/fr/insapp/insapp/ClubActivity.java
@@ -169,7 +169,7 @@ public class ClubActivity extends AppCompatActivity {
 
         Fragment EventsFragment = new EventsFragment();
         Bundle bundle2 = new Bundle();
-        bundle2.putInt("layout", R.layout.row_event_with_avatars);
+        bundle2.putInt("layout", R.layout.row_event);
         bundle2.putString("filter_club_id", club.getId());
         EventsFragment.setArguments(bundle2);
         adapter.addFragment(EventsFragment, getResources().getString(R.string.events));
--- app/src/main/java/fr/insapp/insapp/adapters/PostRecyclerViewAdapter.java
+++ app/src/main/java/fr/insapp/insapp/adapters/PostRecyclerViewAdapter.java
@@ -86,7 +86,7 @@ public class PostRecyclerViewAdapter extends RecyclerView.Adapter<PostRecyclerVi
         holder.date.setText(new String("il y a " + Operation.displayedDate(post.getDate())));
 
         if (layout != R.layout.row_post) {
-            Glide.with(context).load(HttpGet.IMAGEURL + post.getImage()).bitmapTransform(new RoundedCornersTransformation(context, 5, 0)).into(holder.image);
+            Glide.with(context).load(HttpGet.IMAGEURL + post.getImage()).bitmapTransform(new RoundedCornersTransformation(context, 8, 0)).into(holder.image);
             holder.text.setText(post.getDescription());
             holder.likeCounter.setText(Integer.toString(post.getLikes().size()));
             holder.commentCounter.setText(Integer.toString(post.getComments().size()));

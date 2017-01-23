d6560e5e828a45c064dcb9461749ecb9b4428dbb
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-01-23 10:07:01 +0100

    Added club avatar to events

--- app/src/main/java/fr/insapp/insapp/adapters/EventRecyclerViewAdapter.java
+++ app/src/main/java/fr/insapp/insapp/adapters/EventRecyclerViewAdapter.java
@@ -1,23 +1,31 @@
 package fr.insapp.insapp.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-import fr.insapp.insapp.R;
-import fr.insapp.insapp.models.Event;
 import de.hdodenhof.circleimageview.CircleImageView;
+import fr.insapp.insapp.ClubActivity;
+import fr.insapp.insapp.R;
+import fr.insapp.insapp.http.AsyncResponse;
 import fr.insapp.insapp.http.HttpGet;
+import fr.insapp.insapp.models.Club;
+import fr.insapp.insapp.models.Event;
 
 /**
  * Created by thoma on 18/11/2016.
@@ -58,10 +66,59 @@ public class EventRecyclerViewAdapter extends RecyclerView.Adapter<EventRecycler
     }
 
     @Override
-    public void onBindViewHolder(EventRecyclerViewAdapter.EventViewHolder holder, int position) {
+    public void onBindViewHolder(final EventRecyclerViewAdapter.EventViewHolder holder, int position) {
         final Event event = events.get(position);
 
-        Glide.with(context).load(HttpGet.IMAGEURL + event.getImage()).into(holder.image);
+        if (layout == R.layout.row_event_with_avatars) {
+            final Club club = HttpGet.clubs.get(event.getAssociation());
+
+            if (club == null) {
+
+                HttpGet request = new HttpGet(new AsyncResponse() {
+
+                    public void processFinish(String output) {
+                        if (!output.isEmpty()) {
+                            try {
+                                JSONObject jsonobject = new JSONObject(output);
+
+                                final Club club = new Club(jsonobject);
+                                HttpGet.clubs.put(club.getId(), club);
+
+                                // glide
+
+                                if (layout != R.layout.post)
+                                    Glide.with(context).load(HttpGet.IMAGEURL + club.getProfilPicture()).into(holder.avatar);
+
+                                holder.avatar.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        context.startActivity(new Intent(context, ClubActivity.class).putExtra("club", club));
+                                    }
+                                });
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                });
+                request.execute(HttpGet.ROOTASSOCIATION + "/" + event.getAssociation() + "?token=" + HttpGet.credentials.getSessionToken());
+
+            } else {
+                // glide
+                if (layout != R.layout.post)
+                    Glide.with(context).load(HttpGet.IMAGEURL + club.getProfilPicture()).into(holder.avatar);
+
+                holder.avatar.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        context.startActivity(new Intent(context, ClubActivity.class).putExtra("club", club));
+                    }
+                });
+            }
+        }
+
+        Glide.with(context).load(HttpGet.IMAGEURL + event.getImage()).into(holder.thumbnail);
 
         holder.name.setText(event.getName());
 
@@ -96,20 +153,21 @@ public class EventRecyclerViewAdapter extends RecyclerView.Adapter<EventRecycler
     }
 
     public static class EventViewHolder extends RecyclerView.ViewHolder {
-        //public ImageView thumbnail;
+        public ImageView thumbnail;
         public TextView name;
-        public CircleImageView image;
+        public CircleImageView avatar;
         public TextView date;
         public TextView participants;
 
         public EventViewHolder(View view) {
             super(view);
 
-            //this.thumbnail = (ImageView) view.findViewById(R.id.thumbnail_event);
+            this.avatar = (CircleImageView) view.findViewById(R.id.avatar_club_event);
+
             this.name = (TextView) view.findViewById(R.id.name_event);
-            this.image = (CircleImageView) view.findViewById(R.id.avatar_club_event);
             this.date = (TextView) view.findViewById(R.id.date_event);
             this.participants = (TextView) view.findViewById(R.id.going_event);
+            this.thumbnail = (ImageView) view.findViewById(R.id.thumbnail_event);
         }
 
         public void bind(final Event event, final OnEventItemClickListener listener) {

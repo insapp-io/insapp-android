957a74263bb150189030f79de3f9a7c4878fcad7
acrochet95 <acrochet95@gmail.com>
2017-01-28 15:41:21 +0100

    Few changes

--- app/src/main/java/fr/insapp/insapp/fragments/PostsFragment.java
+++ app/src/main/java/fr/insapp/insapp/fragments/PostsFragment.java
@@ -9,7 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -96,8 +95,6 @@ public class PostsFragment extends Fragment implements SwipeRefreshLayout.OnRefr
 
                 int id = adapter.getPosts().indexOf(post);
                 adapter.updatePost(id, post);
-
-                Toast.makeText(getActivity(), "Fragment Got it: " + requestCode + ", " + resultCode + " " + id, Toast.LENGTH_LONG).show();
             }
         }
     }

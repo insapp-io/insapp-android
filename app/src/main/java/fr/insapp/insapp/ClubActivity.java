f896b5b6d6bc0df9f004bd9831cb4dd36c0e48b1
Thomas Bouvier <tomatrocho@gmail.com>
2017-02-24 14:35:59 +0100

    Added black-transparent status bar

--- app/src/main/java/fr/insapp/insapp/ClubActivity.java
+++ app/src/main/java/fr/insapp/insapp/ClubActivity.java
@@ -156,7 +156,7 @@ public class ClubActivity extends AppCompatActivity {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.black_trans80));
+            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.transparent_black));
         }
 
         // view pager

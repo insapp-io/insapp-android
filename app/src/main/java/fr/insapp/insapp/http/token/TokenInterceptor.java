687270ec4a4ab62a769917b464ca113f9e6d7284
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2017-07-13 14:59:51 +0200

    Added session token auto-refresh when expired

--- app/src/main/java/fr/insapp/insapp/http/token/TokenInterceptor.java
+++ app/src/main/java/fr/insapp/insapp/http/token/TokenInterceptor.java
@@ -1,17 +1,23 @@
 package fr.insapp.insapp.http.token;
 
 import android.content.SharedPreferences;
-import android.content.pm.PackageInstaller;
 
 import com.google.gson.Gson;
 
 import java.io.IOException;
 
+import fr.insapp.insapp.http.retrofit.Client;
+import fr.insapp.insapp.http.retrofit.ServiceGenerator;
+import fr.insapp.insapp.http.retrofit.TypeAdapter;
+import fr.insapp.insapp.models.SessionToken;
+import fr.insapp.insapp.models.User;
+import fr.insapp.insapp.models.credentials.LoginCredentials;
 import fr.insapp.insapp.models.credentials.SessionCredentials;
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import retrofit2.Call;
 
 /**
  * Created by thomas on 10/07/2017.
@@ -42,16 +48,21 @@ public class TokenInterceptor implements Interceptor {
 
         // does the token need to be refreshed ? (unauthorized)
 
-        /*
         if (response.code() == 401) {
-            String newToken = tokenManager.refreshToken();
+            LoginCredentials loginCredentials = new Gson().fromJson(preferences.getString("login", ""), LoginCredentials.class);
 
-            HttpUrl url = request.url().newBuilder().addQueryParameter("token", newToken).build();
+            TypeAdapter loginCredentialsTypeAdapter = new TypeAdapter("credentials", LoginCredentials.class);
+            TypeAdapter sessionTokenTypeAdapter = new TypeAdapter("sessionToken", SessionToken.class);
+            TypeAdapter userTypeAdapter = new TypeAdapter("user", User.class);
+
+            Call<SessionCredentials> call = ServiceGenerator.createService(Client.class, loginCredentialsTypeAdapter, sessionTokenTypeAdapter, userTypeAdapter).logUser(loginCredentials);
+            SessionCredentials refreshedSessionCredentials = call.execute().body();
+
+            HttpUrl url = request.url().newBuilder().addQueryParameter("token", refreshedSessionCredentials.getSessionToken().getToken()).build();
             request = request.newBuilder().url(url).build();
 
             return chain.proceed(request);
         }
-        */
 
         return response;
     }

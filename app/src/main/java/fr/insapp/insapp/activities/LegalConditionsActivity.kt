c98871b387a5a4ec5b477db1704cc798aa9fb0fc
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2018-03-30 00:00:25 +0200

    Updated dependencies + converted two Java files to Kotlin

---
+++ app/src/main/java/fr/insapp/insapp/activities/LegalConditionsActivity.kt
@@ -0,0 +1,43 @@
+package fr.insapp.insapp.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.MenuItem
+import android.view.View
+import android.webkit.WebView
+
+import fr.insapp.insapp.R
+import fr.insapp.insapp.http.ServiceGenerator
+
+/**
+ * Created by thomas on 13/12/2016.
+ */
+
+class LegalConditionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_legal_conditions)
+
+        val toolbar = findViewById<View>(R.id.toolbar_conditions) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setHomeButtonEnabled(true)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        val webView = findViewById<View>(R.id.webview_conditions) as WebView
+        webView.loadUrl(ServiceGenerator.ROOT_URL + "legal")
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            android.R.id.home -> {
+                onBackPressed()
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
